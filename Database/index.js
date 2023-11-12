2c218fdc80b5fc7ec0979533d3faa74bf49b7dad
carlosfya <100475175@alumnos.uc3m.es>
2023-11-12 16:23:31 -0500

    fifth commit

--- Database/index.js
+++ Database/index.js
@@ -1,14 +1,10 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
-const courses = require("./courses.json");
-const modules = require("./modules.json");
-const assignments = require("./assignments.json");
-const users = require("./users.json");
-const grades = require("./grades.json");
-const enrollments = require("./enrollments.json");
-
-const data = {
+import courses from "./courses.json" assert { type: "json" };
+import modules from "./modules.json" assert { type: "json" };
+import assignments from "./assignments.json" assert { type: "json" };
+import users from "./users.json" assert { type: "json" };
+import grades from "./grades.json" assert { type: "json" };
+import enrollments from "./enrollments.json" assert { type: "json" };
+export default {
   courses,
   modules,
   assignments,
@@ -16,8 +12,3 @@ const data = {
   grades,
   enrollments,
 };
-
-export default data;
-
-
-
